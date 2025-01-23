b94e2bfe2e54cb59549b2bedfdf757358d5da307
Bhagya Adikari <kumarini.adikari@gmail.com>
2025-01-23 14:07:23 +0530

    first commit

---
+++ lib/views/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Row(
+            children: <Widget>
+            [Text("Flutter"),
+            Text("News",style: TextStyle(color: Colors.blue),)],
+          ),
+          elevation: 0.0,
+        ),    );
+
+  }
+}
